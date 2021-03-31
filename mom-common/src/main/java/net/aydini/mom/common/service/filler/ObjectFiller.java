691e13c1ef543e33e8c66662abec21bfd14fed6b
Aydin Nasrollahpour <paakro@gmail.com>
2021-03-31 23:48:01 +0430

    add object filler

---
+++ mom-common/src/main/java/net/aydini/mom/common/service/filler/ObjectFiller.java
@@ -0,0 +1,17 @@
+package net.aydini.mom.common.service.filler;
+
+import java.lang.reflect.Field;
+
+import net.aydini.mom.common.holder.MaperEntity;
+
+/**
+ * 
+ * @author <a href="mailto:hi@aydini.net">Aydin Nasrollahpour </a>
+ *
+ * Mar 31, 2021
+ */
+public interface ObjectFiller
+{
+
+    public <T> void fill(MaperEntity<T> maperEntity , Field field);
+}
