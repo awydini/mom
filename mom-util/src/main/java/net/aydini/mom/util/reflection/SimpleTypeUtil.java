f9b82c4aaea47902a7df7605be8fc7fed6b2bd39
Aydin Nasrollahpour <paakro@gmail.com>
2021-04-08 10:10:00 +0430

    refactor

--- mom-util/src/main/java/net/aydini/mom/util/reflection/SimpleTypeUtil.java
+++ mom-util/src/main/java/net/aydini/mom/util/reflection/SimpleTypeUtil.java
@@ -2,6 +2,8 @@ package net.aydini.mom.util.reflection;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,31 +12,30 @@ import java.util.List;
  * @author <a href="mailto:hi@aydini.net">Aydin Nasrollahpour </a>
  *
  */
-public class SimpleTypeUtil {
-	
+public class SimpleTypeUtil
+{
 
-    private final  List<Class<?>> SIMPLE_TYPE_LIST;
-    
+    private final List<Class<?>> SIMPLE_TYPE_LIST;
+
     private static SimpleTypeUtil instance;
 
     SimpleTypeUtil()
     {
-        SIMPLE_TYPE_LIST = Arrays.asList(new Class[]{Byte.class,Short.class,Integer.class,Float.class,Double.class,Boolean.class,Long.class, BigDecimal.class, BigInteger.class,String.class,java.util.Date.class,java.sql.Date.class});
+        SIMPLE_TYPE_LIST = Arrays.asList(new Class[] { Byte.class, Short.class, Integer.class, Float.class, Double.class, Boolean.class,
+                Long.class, BigDecimal.class, BigInteger.class, String.class, java.util.Date.class, java.sql.Date.class, LocalDate.class,
+                LocalTime.class, Object.class });
     }
 
-	private static   SimpleTypeUtil getInstance() {
-		
-		if(instance == null)
-			synchronized (instance) {
-				if(instance == null)
-					instance = new SimpleTypeUtil();
-			}
-		return instance;
-	}
+    private static SimpleTypeUtil getInstance()
+    {
+
+        if (instance == null) instance = new SimpleTypeUtil();
+        return instance;
+    }
 
     public static boolean isSimpleType(Class<?> clazz)
     {
-    	return getInstance().SIMPLE_TYPE_LIST.contains(clazz);
+        return getInstance().SIMPLE_TYPE_LIST.contains(clazz);
     }
-    
+
 }
