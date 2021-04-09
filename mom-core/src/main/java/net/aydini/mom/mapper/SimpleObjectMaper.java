a99cdfca96d5d77070084b053f871594ddf4cf36
Aydin Nasrollahpour <paakro@gmail.com>
2021-04-10 00:37:40 +0430

    add simple ObjectMaper

---
+++ mom-core/src/main/java/net/aydini/mom/mapper/SimpleObjectMaper.java
@@ -0,0 +1,38 @@
+package net.aydini.mom.mapper;
+
+import java.lang.reflect.Field;
+import java.util.Set;
+
+import net.aydini.mom.common.service.filler.FieldFiller;
+import net.aydini.mom.filler.SimpleFieldFiller;
+import net.aydini.mom.util.reflection.FieldWarehouse;
+
+/**
+ * 
+ * @author <a href="mailto:hi@aydini.net">Aydin Nasrollahpour </a>
+ *
+ * Apr 10, 2021
+ */
+public class SimpleObjectMaper extends AbstractObjectMaper
+{
+
+    private final FieldFiller fieldFiller; 
+    
+    public SimpleObjectMaper()
+    {
+        this.fieldFiller = new SimpleFieldFiller(this);
+    }
+
+    @Override
+    public FieldFiller gettFieldFiller()
+    {
+        return fieldFiller;
+    }
+
+    @Override
+    protected <T> Set<Field> getMapingFields(Class<T> targetClass)
+    {
+        return FieldWarehouse.getClassFields(targetClass);
+    }
+    
+}
---
+++ mom-core/src/test/java/net/aydini/mom/mapper/SimpleObjectMaperTests.java
@@ -0,0 +1,46 @@
+package net.aydini.mom.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import net.aydini.mom.model.Person;
+import net.aydini.mom.model.User;
+import net.aydini.mom.model.UserDto;
+
+public class SimpleObjectMaperTests
+{
+
+    private SimpleObjectMaper objectMaper;
+    private UserDto userDto;
+    
+    @BeforeEach
+    public void init()
+    {
+        objectMaper = new SimpleObjectMaper();
+        userDto = new UserDto("aydin","123",LocalDate.now());
+        Person person = new Person();
+        person.setAge(18);
+        person.setName("joe");
+        userDto.setPerson(person);
+    }
+    
+    
+    @Test
+    public void objectMaperTests()
+    {
+        User user = objectMaper.map(userDto, User.class);
+        assertEquals(userDto.getUsername(), user.getUsername());
+        assertEquals(userDto.getPassword(), user.getPassword());
+        assertEquals(userDto.getRegisterDate(), user.getRegisterDate());
+        assertNotNull(user.getPerson());
+        assertEquals(userDto.getPerson().getName(), user.getPerson().getName());
+        assertEquals(userDto.getPerson().getAge(), user.getPerson().getAge());
+        
+    }
+    
+}
