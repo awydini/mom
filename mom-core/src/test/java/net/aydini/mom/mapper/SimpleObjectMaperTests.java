2bac8edf30728c735bec9e7c6af21ad4d5e5fa7f
Aydin Nasrollahpour <paakro@gmail.com>
2021-04-11 13:43:48 +0430

    update tests

--- mom-core/src/test/java/net/aydini/mom/mapper/SimpleObjectMaperTests.java
+++ mom-core/src/test/java/net/aydini/mom/mapper/SimpleObjectMaperTests.java
@@ -11,51 +11,38 @@ import org.junit.jupiter.api.Test;
 import net.aydini.mom.model.Person;
 import net.aydini.mom.model.SuperUser;
 import net.aydini.mom.model.User;
-import net.aydini.mom.model.UserDto;
 
 public class SimpleObjectMaperTests
 {
 
     private SimpleObjectMaper objectMaper;
-    private UserDto userDto;
-    
+    private User user;
+
     @BeforeEach
     public void init()
     {
         objectMaper = new SimpleObjectMaper();
-        userDto = new UserDto("aydin","123",LocalDate.now());
+        user = new User("aydin", "123", LocalDate.now());
         Person person = new Person();
         person.setAge(18);
         person.setName("joe");
-        userDto.setPerson(person);
-    }
-    
-    
-    @Test
-    public void objectMaperTest()
-    {
-        User user = objectMaper.map(userDto, User.class);
-        assertEquals(userDto.getUsername(), user.getUsername());
-        assertEquals(userDto.getPassword(), user.getPassword());
-        assertEquals(userDto.getRegisterDate(), user.getRegisterDate());
-        assertNotNull(user.getPerson());
-        assertEquals(userDto.getPerson().getName(), user.getPerson().getName());
-        assertEquals(userDto.getPerson().getAge(), user.getPerson().getAge());
-        
+        user.setPerson(person);
     }
 
-    
-    
     @Test
     public void objectMaperTestWithDifferentPersonType()
     {
-        SuperUser user = objectMaper.map(userDto, SuperUser.class);
-        assertEquals(userDto.getUsername(), user.getUsername());
-        assertEquals(userDto.getPassword(), user.getPassword());
-        assertEquals(userDto.getRegisterDate(), user.getRegisterDate());
+
+        long start = System.currentTimeMillis();
+        SuperUser superUser = objectMaper.map(user, SuperUser.class);
+        System.out.println(System.currentTimeMillis() - start);
+
+        assertEquals(user.getUsername(), superUser.getUsername());
+        assertEquals(user.getPassword(), superUser.getPassword());
+        assertEquals(user.getRegisterDate(), superUser.getRegisterDate());
         assertNotNull(user.getPerson());
-        assertEquals(userDto.getPerson().getName(), user.getPerson().getName());
-        assertEquals(userDto.getPerson().getAge(), user.getPerson().getAge());
-        
+        assertEquals(user.getPerson().getName(), superUser.getPerson().getName());
+        assertEquals(user.getPerson().getAge(), superUser.getPerson().getAge());
+
     }
 }
